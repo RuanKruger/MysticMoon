d98021b885d4dcf3908de5a49b51589c2b5b3aee
Ruan Kruger <2021793@TRUWORTHS.CO.ZA>
2017-07-13 12:37:11 +0200

    Siphe Commit

--- src/app/landingPage/contact-us/contact-us.component.ts
+++ src/app/landingPage/contact-us/contact-us.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
+   
+  user = {name: '', email: '', telephone: '', message: ''}
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  test() {
+    console.log(this.user.name);
+    console.log(this.user.email);
+    console.log(this.user.telephone);
+    console.log(this.user.message);
+  }
 }
